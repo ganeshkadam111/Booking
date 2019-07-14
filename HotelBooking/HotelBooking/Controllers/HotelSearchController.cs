b0cafb1b22a7ea012e013443bfb7130573c7f9cf
ganesh <ganeshkadam96@gmail.com>
2019-07-14 19:09:39 +0530

    Payment and Traveller controller

---
+++ HotelBooking/HotelBooking/Controllers/HotelSearchController.cs
@@ -0,0 +1,53 @@
+﻿using HotelBooking.Common;
+using HotelBooking.Models;
+using HotelBooking.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelBooking.Controllers
+{
+    public class HotelSearchController : Controller
+    {
+        private IHotelRepository hotelRepository = null;
+
+        public HotelSearchController()
+        {
+            this.hotelRepository = new HotelRepository();
+
+        }
+        // GET: HotelSearch
+        public ActionResult HotelSearchBooking()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult HotelSearchBookingList(string keyword)
+        {
+            //Searching records from list using LINQ query  
+            var searchHotelList = GetHotelSearchList(keyword);
+
+            return Json(searchHotelList, JsonRequestBehavior.AllowGet);
+        }
+
+        private IEnumerable<HotelViewModel> GetHotelSearchList(string searchValue)
+        {
+            try
+            {
+                IEnumerable<HotelViewModel> searchHotelList = hotelRepository.GetAllHotels().Where(f =>
+                          f.CityName.ToLower().Contains(searchValue.ToLower()) ||
+                          f.CountryName.ToLower().Contains(searchValue.ToLower()));
+                return searchHotelList;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message.ToString());
+                return null;
+            }
+        }
+
+    }
+}
---
+++ HotelBooking/HotelBooking/Controllers/TravellerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelBooking.Controllers
+{
+    public class TravellerController : Controller
+    {
+        // GET: Traveller
+        public ActionResult AddTraveller()
+        {
+            return View();
+        }
+
+    }
+}
